5e24f8347c52cfd9c8225f3a18f3fae85ea7f6b2
Katelyn-Majaury <71095330+Katelyn-Majaury@users.noreply.github.com>
2020-11-14 23:03:36 -0500

    Create Lab9-Challenge3.js

---
+++ Lab9-Challenge3.js
@@ -0,0 +1,4 @@
+function total(){var inputone = document.getElementById("Inputnumber1").value;
+var inputtwo = document.getElementById("Inputnumber2").value;
+var addition = Number(inputone) + Number(inputtwo);
+document.getElementById("Outputnumber").innerHTML = addition;}
