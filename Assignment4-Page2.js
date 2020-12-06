52d3c5b2bf6df29aa3f49067da73354856afcc43
Katelyn-Majaury <71095330+Katelyn-Majaury@users.noreply.github.com>
2020-12-06 14:34:35 -0500

    Create Assignment4-Page2.js

---
+++ Assignment4-Page2.js
@@ -0,0 +1,4 @@
+var favouriteBook = prompt ("What is your Favourite Book?");
+var txt = "Wow!" + " "+ favouriteBook + " " + "sounds interesting!";
+
+document.getElementById("prompt1").innerHTML = txt;
